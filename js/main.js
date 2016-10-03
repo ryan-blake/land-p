69a155883577194de3e7d3c18c7d9a2b3c360272
Ryan Kliemann <Ryan_blake@icloud.com>
2016-10-03 10:13:07 -0500

    update pages

--- js/main.js
+++ js/main.js
@@ -8,3 +8,50 @@ if ($(this).scrollTop() > 1){
     $('.navigation').removeClass("sticky");
   }
 });
+
+$( document ).ready( function() {
+
+
+	//Google Maps JS
+	//Set Map
+	function initialize() {
+			var myLatlng = new google.maps.LatLng(32.947919, -97.116972);
+			var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
+			var mapOptions = {
+				zoom: 11,
+				center: myLatlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+
+		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+		//Callout Content
+		var contentString = '500 N Kimball Ave #109, Southlake, TX 76092';
+		//Set window width + content
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString,
+			maxWidth: 300
+		});
+
+		//Add Marker
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			icon: imagePath,
+			title: ''
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+
+		//Resize Function
+		google.maps.event.addDomListener(window, "resize", function() {
+			var center = map.getCenter();
+			google.maps.event.trigger(map, "resize");
+			map.setCenter(center);
+		});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+});
